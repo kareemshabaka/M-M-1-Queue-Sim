3cd92c673dea735a0589325b78524973cef8bd6a
Kareem <kareem.shabaka@gmail.com>
2022-10-16 02:51:45 -0400

    Tweaked spawn timers and added interactable buttons to change various parameters

--- queueSim.py
+++ queueSim.py
@@ -2,6 +2,7 @@ import math
 import pygame
 import numpy as np
 import pandas as pd
+from numpy.random import default_rng
 
 clock = pygame.time.Clock()
 pygame.init()
@@ -12,17 +13,22 @@ pygame.freetype.init()
 titleFont = pygame.font.SysFont("arial", 40)
 bodyFont = pygame.font.SysFont("arial", 30)
 class Simulation:
-    def __init__(self,tellers,customers):
+    def __init__(self,tellers,customers,multiplier=40):
         self.queueLength = 0
         self.tellers = tellers
         self.numTellers = len(self.tellers)
         self.customers = customers
         self.nextArrival = 0
         self.beingServed = [None]*self.numTellers
-
+        self.multiplier=multiplier
         self.totalCustomers = 0
         self.customersPerTeller=[0]*self.numTellers
         
+        self.buttons = []
+
+        self.arrivalTime = titleFont.render("Arrival Time: {}".format(str(self.multiplier)), True, (0, 0, 0))
+        self.increaseTellers = titleFont.render("Number of Tellers", True, (0, 0, 0))
+        self.tellerTime = titleFont.render("Teller Processing Time ", True, (0, 0, 0))
     def serve(self,customerID):
         tellerID=-1
         for teller in self.tellers:
@@ -36,10 +42,13 @@ class Simulation:
         return False, tellerID
         
     def arrival(self):
+        if self.multiplier<=0:
+            self.multiplier==0.1   
         self.queueLength+=1
         newCustomer = Customer(self.queueLength)
         self.customers.append(newCustomer)
-        self.nextArrival = simtime + (-np.log(1-(np.random.uniform(low=0.0,high=1.0))) * 3)*10
+        self.nextArrival = simtime + (-np.log(1-(np.random.uniform(low=0.0,high=1.0)))/self.multiplier*60*60)
+        print(self.nextArrival)
 
 
     def departure(self,tellerID):
@@ -76,12 +85,19 @@ class Simulation:
         display.blit(self.teller2,(20, 220 ))
         self.t2Answer = titleFont.render(str(self.customersPerTeller[1]), True, (0, 0, 0))
         display.blit(self.t2Answer,(400 , 220 ))
-'''
-        self.queueL = titleFont.render("Queue Length:", True, (0, 0, 0))
-        display.blit(self.teller2,(20, 220 ))
-        self.queueLAnswer = titleFont.render(str(self.customersPerTeller[1]), True, (0, 0, 0))
-        display.blit(self.t2Answer,(400 , 220 ))
-'''
+
+        self.qL = titleFont.render("Queue Length: {0}".format(str(self.queueLength)), True, (0,0,0))
+        display.blit(self.qL,(520,100))
+
+        self.arrivalTime = titleFont.render("Arrivals per hour: {}".format(str(self.multiplier)), True, (0, 0, 0))
+        display.blit(self.arrivalTime,(60, 400 ))
+        display.blit(self.increaseTellers,(60 , 460 ))
+        display.blit(self.tellerTime,(60, 520 ))
+
+        self.timePassed = titleFont.render("Time: {0}s".format(str(simtime)), True, (0,0,0))
+        display.blit(self.timePassed,(800,5))
+        
+
 
 class Teller():
     def __init__(self, multiplier,tellerID):
@@ -92,9 +108,13 @@ class Teller():
         self.tellerID = tellerID
         
 
-    def tellerDepartureTime(self):                               
-        self.delay = (-np.log(1-(np.random.uniform(low=0.0,high=1.0)))*self.multiplier)*20
-        #print(self.delay)
+    def tellerDepartureTime(self):
+        if self.multiplier<=0:
+            self.multiplier==0             
+
+        rg = default_rng()
+        rg.poisson(5)              
+        self.delay = (-np.log(1-(np.random.uniform(low=0.0,high=1.0)))*self.multiplier)*45
         self.finishService = simtime + self.delay
         self.state=1
 
@@ -151,7 +171,20 @@ class Customer():
                     self.isServed+=1
             
 
-        
+class Button():
+    def __init__(self,text,pos,use):
+        self.x,self.y=pos        
+        self.use=use
+        self.text = titleFont.render(text, 1, pygame.Color("red"))
+        self.size = self.text.get_size()
+        self.surface = pygame.Surface(self.size)
+        self.surface.fill("white")
+        self.surface.blit(self.text, (0, 0))
+        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+
+    def draw(self):
+        screen.blit(self.surface, (self.x, self.y))
+
 def main():
     running = True
 
@@ -162,21 +195,50 @@ def main():
     simtime = 0
     sim = Simulation([teller1,teller2], [])
 
+    arrivalm = Button("-",(35, 400),"decreaseAT")
+    arrivalp = Button("+",(sim.arrivalTime.get_size()[0]+170, 400),"increaseAT")
+    
+    ntellerm = Button("-",(35, 460),"decreaseTeller")
+    ntellerp = Button("+",(sim.increaseTellers.get_size()[0]+70, 460),"increaseTeller")
+
+    tellertimem = Button("-",(35, 520),"decreaseTT")
+    tellertimep = Button("+",(sim.tellerTime.get_size()[0]+70, 520),"increaseTT")
+
+    sim.buttons = [arrivalm,arrivalp,ntellerm,ntellerp,tellertimem,tellertimep]
+
     while running:
-        clock.tick(10)
+        clock.tick(60)       
         if sim.nextArrival <= simtime: #new customers arrive
            sim.arrival()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                #print(x,y)
+                for button in sim.buttons:
+                    if button.rect.collidepoint(x, y):
+                        #button.click()
+                        if button.use == "decreaseAT":
+                            sim.multiplier-=0.5
+                        if button.use == "increaseAT":
+                            sim.multiplier+=0.5
+                        if button.use == "decreaseTT":
+                            for teller in sim.tellers:
+                                teller.multiplier-=0.3
+                        if button.use == "increaseTT":
+                            for teller in sim.tellers:
+                                teller.multiplier+=0.3
         screen.fill((255, 255, 255))
         
         sim.draw(screen)
     
         sim.UI(screen)
 
+        for button in sim.buttons:
+            button.draw()
         if (simtime%1==0):
             for customer in sim.customers:
                 if customer.queueLocation == 1 and customer.isServed==0 and customer.ready==1:
@@ -191,7 +253,6 @@ def main():
             
         for customer in sim.customers:
             customer.update(sim,customer.assignedTeller)
-            #print(customer.assignedTeller)
             customer.draw(screen)
 
         for teller in sim.tellers:
